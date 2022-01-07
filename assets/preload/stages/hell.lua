735e4a0e888a3aba4164ddfa211f473dd8ba672a
zndndnxnxnxnxnxnxnxn <82340501+zndndnxnxnxnxnxnxnxn@users.noreply.github.com>
2022-01-07 10:02:35 -0600

    Add files via upload

---
+++ assets/preload/stages/hell.lua
@@ -0,0 +1,11 @@
+function onCreate()
+
+
+	makeAnimatedLuaSprite('bg','Fire',0,100)
+	addAnimationByPrefix('bg','animated','Fire',24,true)
+	makeLuaSprite('fg','Ground Fire',-1000,700)
+
+	addLuaSprite('bg',false)
+	addLuaSprite('fg',false)
+	close(true);
+end --lol idk why jose had troubles with the bgs its so fun loollllllllll!!
